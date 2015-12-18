9e2ce06af3fbec9749d82d8db8ec7a461ac5019d
Ikram_Lim <ikramlim@gmail.com>
2015-12-19 00:41:31 +0800

    descending_order_array
    descending_order_array

---
+++ descending_order_array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace descending_order_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 2, 4, 1, 6 };
+            Array.Sort(a);
+            Array.Reverse(a);
+            foreach(var str in a)
+            {
+                Console.Write(str.ToString());
+            }
+        }
+    }
+}
